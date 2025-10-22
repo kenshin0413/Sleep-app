8b961c3c2c51387fc10d175978bd9fab0917d763
kenshin0413 <kenshin1234569@gmail.com>
2025-10-22 16:20:14 +0900

    初期

---
+++ Sleep-app/Sleep-app/Sleep_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_appApp.swift
+//  Sleep-app
+//
+//  Created by miyamotokenshin on R 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
